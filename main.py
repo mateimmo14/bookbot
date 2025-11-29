eb85829e0a8bc8a6404b109511e344a5b0bc9006
mateimmo14 <mateimmo14@gmail.com>
2025-11-29 15:08:40 +0200

    FINALLY FUCKING FIXED LETTER COUNTER

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+from stats import count_characters, count_words
+
+
 def get_book_text(filepath):
     with open(filepath) as text:
         contents = text.read()
     return contents
 
 
-def count_words(book_text):
-    book_words = book_text.split()
-    num_words = 0
-    for word in book_words:
-        num_words += 1
-    print(f"Found {num_words} total words")
-
-
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     count_words(book_text)
+    char_counte = count_characters(book_text)
+    print(char_counte)
+
     return
 
 
---
+++ test.py
@@ -0,0 +1,4 @@
+x = "thou dof shalt shat thous opants"
+for word in x:
+    for letter in word:
+        print(letter)
